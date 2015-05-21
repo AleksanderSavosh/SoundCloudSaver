ded62627e20eac7a503f405f7a1b5719d9eaf65d
savosh <SavoshAlex@gmail.com>
2015-05-21 23:58:45 +0300

    change tabs background

--- src/savosh/soundcloudsaver/MainActivity.java
+++ src/savosh/soundcloudsaver/MainActivity.java
@@ -3,6 +3,10 @@ package savosh.soundcloudsaver;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
     private FragmentTabHost mTabHost;
@@ -17,5 +21,22 @@ public class MainActivity extends FragmentActivity {
 
         mTabHost.addTab(mTabHost.newTabSpec("search").setIndicator("Search"), SearchFragment.class, null);
         mTabHost.addTab(mTabHost.newTabSpec("save").setIndicator("Save"), SaverFragment.class, null);
+
+        setTabsBackground(mTabHost);
     }
+
+    public void setTabsBackground(TabHost tabhost) {
+        TabWidget widget = tabhost.getTabWidget();
+        for(int i = 0; i < widget.getChildCount(); i++) {
+            View v = widget.getChildAt(i);
+            // Look for the title view to ensure this is an indicator and not a divider.
+            TextView tv = (TextView)v.findViewById(android.R.id.title);
+            if(tv == null) {
+                continue;
+            }
+            tv.setTextColor(getResources().getColor(R.color.main_toolbar));
+            v.setBackgroundResource(R.drawable.main_activity_tabs);
+        }
+    }
+
 }
